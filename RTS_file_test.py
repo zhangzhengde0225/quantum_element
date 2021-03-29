d3980d124e99ee142173696ea93f779f1c207a77
zhangzhengde <zhangzhengde0225@gmail.com>
2021-03-30 00:40:30 +0800

    1

--- RTS_file_test.py
+++ RTS_file_test.py
@@ -1,28 +1,28 @@
-
+"""
+manage the output of vicon.
+"""
 # coding: utf-8
 
-import pandas as pd
 import re
-import sys
-
+import pandas as pd
 
 # user-defined
 filename = 'out.txt'
 outfile = 'test.csv'
 
-#condition = ['Object2']
+# condition = ['Object2']
 condition = ['xc_right_hand']
-#condition = ['SBOX']
+# condition = ['SBOX']
 
 with open(filename, 'r') as f:
     raw_rows = f.readlines()
 
-#要求数据大于3个刚体
+# 要求数据大于3个刚体
 first = raw_rows.index('\n')
-second = raw_rows[first+1:].index('\n')
+second = raw_rows[first + 1:].index('\n')
 
 distance = second + 1
-strip_tail = (len(raw_rows)-1)%distance
+strip_tail = (len(raw_rows) - 1) % distance
 rows = raw_rows[2:-strip_tail]
 
 temp = rows[:distance]
@@ -36,14 +36,12 @@ temp = rows[:distance]
 # value_num = 4
 value_num = distance - 1
 
-
-result = [e for i,e in enumerate(rows) if (i%distance <= value_num) and (i%distance > 0)]
-split_result = [re.split(r'[,()]',row)[:10] for row in result]
-add_zero_result = [row[:5]  + row[6:] for row in split_result]
+result = [e for i, e in enumerate(rows) if (i % distance <= value_num) and (i % distance > 0)]
+split_result = [re.split(r'[,()]', row)[:10] for row in result]
+add_zero_result = [row[:5] + row[6:] for row in split_result]
 
 title = ['time', 'name'] + list('xyzabcw')
-df = pd.DataFrame(add_zero_result, columns = title)
+df = pd.DataFrame(add_zero_result, columns=title)
 
-df_out = df[df.name.isin(condition)]   
+df_out = df[df.name.isin(condition)]
 df_out.to_csv(outfile, index=False)
-
